2281f7e5ce3f273018395e3a7bc5fe4565aab93d
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-06-23 23:22:38 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,13 @@
-input number1 = 10
-input number2 = 5
-int sum = number1 + number2
-int subtract = number2 - number1
-int product = number1 * number2
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Perform the operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
