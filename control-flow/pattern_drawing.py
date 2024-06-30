8d77043d78ad165592d4abf6327962b4f6edbc95
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-06-30 19:05:50 +0300

    Create pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+def main():
+    size = int(input("Enter the size of the pattern: "))
+    row = 0
+    while row < size:
+        col = 0
+        while col < size:
+            print("*", end=" ")
+            col += 1
+        print()  
+        row += 1
+if __name__ == "__main__":
+    main()
