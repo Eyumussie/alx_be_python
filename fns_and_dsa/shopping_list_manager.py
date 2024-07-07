444a6bc34e359aa1019447188950b49117f07fc3
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-07-07 21:40:24 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -4,7 +4,7 @@ def display_menu():
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-f= input("input an item")
+item = input("Enter the item  to add: ")
 def main():
     shopping_list = []
     while True:
@@ -12,11 +12,9 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item  to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to the shopping list.")
         elif choice == '2':
-            item = input("Enter the item name to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from the shopping list.")
