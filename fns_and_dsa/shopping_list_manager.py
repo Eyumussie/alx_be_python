3c2cf0ad2ac4f06e16ae76c467d62c00d0ef7436
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-07-07 21:46:50 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,20 +10,14 @@ def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ")
-
         if choice == '1':
             shopping_list.append(item)
             print(f"'{item}' has been added to the shopping list.")
         elif choice == '2':
-            if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from the shopping list.")
-            else:
-                print(f"'{item}' is not in the shopping list.")
         elif choice == '3':
-            print("Current Shopping List:")
-            for idx, item in enumerate(shopping_list, start=1):
-                print(f"{idx}. {item}")
+            print(item:)
         elif choice == '4':
             print("Goodbye!")
             break
