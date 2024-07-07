22269b7b39e048c49dc3bfe9275205d2d9c51c1d
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-07-07 20:31:18 +0300

    Create explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,26 @@
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_date}")
+    return current_date
+
+def calculate_future_date(current_date, days):
+    future_date = current_date + timedelta(days=days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future Date after {days} days: {formatted_future_date}")
+    return future_date
+
+def main():
+    
+    current_date = display_current_datetime()
+
+   
+    try:
+        days = int(input("Enter the number of days to add: "))
+        calculate_future_date(current_date, days)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
